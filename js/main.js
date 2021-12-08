1a0b00031a344f918475f29548a736dccf6fecd5
saadProgram <saadullahhussaini10@gmail.com>
2021-12-08 17:58:06 +0500

    message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const firebaseConfig = {
+
+
+var config = {
     apiKey: "AIzaSyD0JJS5XwLcahOJW5J0hztX_Ila2GYL-ek",
     authDomain: "supermarket-af419.firebaseapp.com",
     databaseURL: "https://supermarket-af419-default-rtdb.firebaseio.com",
@@ -7,23 +9,53 @@ const firebaseConfig = {
     messagingSenderId: "65288925001",
     appId: "1:65288925001:web:dcd6b15832615bbf983498",
     measurementId: "${config.measurementId}"
-  };
-
+  }; firebase.initializeApp(config);
+  
   // Reference messages collection
-var messagesRef = firebase.database().ref('contactformmessages');
-
-$('#contactForm').submit(function(e) {
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
     e.preventDefault();
- 
+  
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, company, email, phone, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message){
     var newMessageRef = messagesRef.push();
     newMessageRef.set({
-        name: $('.fullname').val(),
-        email: $('.email').val(),
-        subject: $('.subject').val(),
-        message: $('.message').val()
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message
     });
- 
-    $('.success-message').show();
- 
-    $('#contactForm')[0].reset();
-});
+  }
